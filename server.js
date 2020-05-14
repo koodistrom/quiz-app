44559b8c4f9b10aba2f25d84182d3efe12b3a1ff
Jaakko <j.i.mantyla@gmail.com>
2020-05-14 19:04:56 +0300

    jeeje

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/triviagame3000'));
+app.use(express.static(__dirname + '/quiz-app'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/triviagame3000/index.html'));
+res.sendFile(path.join(__dirname+'/dist/quiz-app/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
